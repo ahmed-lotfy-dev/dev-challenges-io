69718d921a7ab107f451bc0f8bb8b6f9d31af996
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-12-09 11:53:07 +0200

    Finish random quote generator

---
+++ javascript-roadmap/random-quotes/js/index.js
@@ -0,0 +1,48 @@
+const authorElement = document.querySelector(".author")
+const categoryElement = document.querySelector(".category")
+const quoteElement = document.querySelector(".quote-text")
+const newQuoteElement = document.querySelector(".new-quote")
+const shareQuoteElement = document.querySelector(".share-quote")
+
+let quote
+
+async function fetchQuote() {
+  const response = await fetch("https://api.quotable.io/quotes/random")
+  const quoteData = await response.json()
+  quote = quoteData[0]
+}
+
+function createElements() {
+  if (!quote) {
+    quoteElement.innerHTML = `
+    <p>
+    <span>&ldquo;</span>
+    No Quote To Show At The Moment
+    <span>&rdquo;</span>
+    </p>
+    `
+  }
+  authorElement.innerHTML = `<h1>${quote.author}</h1>`
+  quote.tags.forEach((tag) => {
+    categoryElement.innerHTML = `<p>${tag}</p>`
+  })
+  quoteElement.innerHTML = `
+  <p>
+  <span>&ldquo;</span>
+  ${quote.content}
+  <span>&rdquo;</span>
+  </p>
+  `
+}
+document.addEventListener("DOMContentLoaded", async () => {
+  await fetchQuote()
+  createElements()
+})
+newQuoteElement.addEventListener("click", async () => {
+  await fetchQuote()
+  createElements()
+})
+shareQuoteElement.addEventListener("click", () => {
+  navigator.clipboard.writeText(quote.content)
+  alert("Quote Copied Successfully now share it to your social media")
+})
