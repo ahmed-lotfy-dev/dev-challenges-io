da7fb6c498fa2e770f44ef28fff6d607eb64f33e
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-12-28 18:35:39 +0200

    Finish Country Page with optional pagination

--- front-end-libraries/country-page/src/App.tsx
+++ front-end-libraries/country-page/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useDataContext } from "./AppContext"
 import useFetch from "./hooks/useFetch"
 import { ApiResponse } from "./types"
-import { Outlet } from "react-router"
+import { NavLink, Outlet } from "react-router"
 
 function App() {
   const { setCountriesData, setFilters } = useDataContext()
@@ -21,11 +21,13 @@ function App() {
   return (
     <div className="w-full h-screen relative">
       <div className="w-full h-[300px] bg-[url(/hero-image-wr.jpg)] bg-cover bg-center bg-no-repeat -z-50 border-[0.5px] border-b-componentClr"></div>
+      <NavLink to={"/"}>
       <img
         src="/public/Logo.svg"
         alt=""
         className="absolute top-[13%] left-[50%] -translate-x-[50%] -translate-y-[13%]"
       />
+      </NavLink>
       <Outlet />
     </div>
   )
--- front-end-libraries/country-page/src/components/Table.tsx
+++ front-end-libraries/country-page/src/components/Table.tsx
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router"
 import { useDataContext } from "../AppContext"
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 export default function Table() {
   const { countriesData, filters, setFilters } = useDataContext()
+  const [page, setPage] = useState(1)
 
   const sortedCountries = useMemo(() => {
     if (!countriesData) return []
@@ -60,12 +61,23 @@ export default function Table() {
     filters.query,
   ])
 
+  let perPage = 13
+  const pagesCount = Math.ceil(filteredCountries.length / perPage)
+  const startIndex = (page - 1) * perPage
+  const endIndex = page * perPage
+
   useEffect(() => {
     setFilters((prev) => ({
       ...prev,
       countriesCount: filteredCountries.length,
     }))
   }, [filteredCountries, sortedCountries])
+
+  useEffect(() => {
+    if (page > pagesCount) {
+      setPage(1)
+    }
+  }, [pagesCount, page])
 
   return (
     <div className="w-full m-auto text-textColor col-span-3 md:col-span-2 md:ml-5">
@@ -81,32 +93,65 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {filteredCountries?.map((country, index) => (
-            <tr key={`${country.cca3}-${index}`}>
-              <td className="w-20 pt-6">
-                <NavLink to={`${country.name.common}`}>
-                  <img
-                    className="rounded-md w-20"
-                    src={country.flags.svg}
-                    alt=""
-                  />
-                </NavLink>
-              </td>
-              <td className="text-start">
-                <NavLink to={`${country.name.common}`}>
-                  {country.name.common}
-                </NavLink>
-              </td>
-              <td className="text-start">
-                {new Intl.NumberFormat().format(country.population)}
-              </td>
-              <td className="text-start">
-                {new Intl.NumberFormat().format(country.area)}
-              </td>
-            </tr>
-          ))}
+          {filteredCountries
+            .slice(startIndex, endIndex)
+            ?.map((country, index) => (
+              <tr key={`${country.cca3}-${index}`}>
+                <td className="w-20 pt-6">
+                  <NavLink to={`${country.name.common}`}>
+                    <img
+                      className="rounded-md w-24 h-24"
+                      src={country.flags.svg}
+                      alt=""
+                    />
+                  </NavLink>
+                </td>
+                <td className="text-start">
+                  <NavLink to={`${country.name.common}`}>
+                    {country.name.common}
+                  </NavLink>
+                </td>
+                <td className="text-start">
+                  {new Intl.NumberFormat().format(country.population)}
+                </td>
+                <td className="text-start">
+                  {new Intl.NumberFormat().format(country.area)}
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div className="flex justify-center my-10 gap-2">
+        <button
+          className={`px-3 py-1 border rounded-md ${
+            page ? "bg-activeBgFilter text-white" : "bg-white"
+          }`}
+          onClick={() => {
+            if (page > 1) setPage(page - 1)
+            else return
+          }}
+        >
+          previous
+        </button>
+        <button
+          className={`px-3 py-1 border rounded-md ${
+            page ? "bg-activeBgFilter text-white" : "bg-white"
+          }`}
+        >
+          {page}
+        </button>
+        <button
+          className={`px-3 py-1 border rounded-md ${
+            page ? "bg-activeBgFilter text-white" : "bg-white"
+          }`}
+          onClick={() => {
+            if (page == pagesCount) return
+            else setPage(page + 1)
+          }}
+        >
+          next
+        </button>
+      </div>
     </div>
   )
 }
